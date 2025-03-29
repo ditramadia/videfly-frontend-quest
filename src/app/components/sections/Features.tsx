57e0fe835cc6634cca777bbce7440beb8ab84a3f
Ditra Amadia <ditrarizqaamadia@gmail.com>
2025-03-30 01:39:26 +0700

    feat: add support section animation

--- src/app/components/sections/Features.tsx
+++ src/app/components/sections/Features.tsx
@@ -36,7 +36,7 @@ const Features = () => {
             initial={{ x: "-350%"}}
             animate={isInView ? { x: 0 } : {}}
             transition={{ duration: 1.5, ease:  "easeInOut" }}
-            className="basis-full md:basis-1/2 lg:basis-1/3 p-1 md:p-2"
+            className="basis-full md:basis-1/2 lg:basis-1/3 py-2 md:p-2"
           >
             <FeatureCard feature={features[0]} icon={LinkIcon} />
           </motion.div>
@@ -44,7 +44,7 @@ const Features = () => {
             initial={{ x: "-350%"}}
             animate={isInView ? { x: 0 } : {}}
             transition={{ duration: 1.5, ease:  "easeInOut" }}
-            className="basis-full md:basis-1/2 lg:basis-1/3 p-1 md:p-2"
+            className="basis-full md:basis-1/2 lg:basis-1/3 py-2 md:p-2"
           >
             <FeatureCard feature={features[1]} icon={TemplateIcon} />
           </motion.div>
@@ -52,7 +52,7 @@ const Features = () => {
             initial={{ x: "-350%"}}
             animate={isInView ? { x: 0 } : {}}
             transition={{ duration: 1.5, ease:  "easeInOut" }}
-            className="basis-full md:basis-1/2 lg:basis-1/3 p-1 md:p-2"
+            className="basis-full md:basis-1/2 lg:basis-1/3 py-2 md:p-2"
           >
             <FeatureCard feature={features[2]} icon={EditIcon} />
           </motion.div>
@@ -60,7 +60,7 @@ const Features = () => {
             initial={{ x: "250%"}}
             animate={isInView ? { x: 0 } : {}}
             transition={{ duration: 1.5, ease:  "easeInOut" }}
-            className="basis-full md:basis-1/2 p-1 md:p-2"
+            className="basis-full md:basis-1/2 py-2 md:p-2"
           >
             <FeatureCard feature={features[3]} icon={ChartIcon} />
           </motion.div>
@@ -68,7 +68,7 @@ const Features = () => {
             initial={{ x: "250%"}}
             animate={isInView ? { x: 0 } : {}}
             transition={{ duration: 1.5, ease:  "easeInOut" }}
-            className="basis-full lg:basis-1/2 lg:w-1/2 p-1 md:p-2"
+            className="basis-full lg:basis-1/2 lg:w-1/2 py-2 md:p-2"
           >
             <FeatureCard feature={features[4]} icon={CalendarIcon} />
           </motion.div>
