113c69d051a799d62721b6332b26a2b6c1b18e67
Ditra Amadia <ditrarizqaamadia@gmail.com>
2025-03-29 16:24:10 +0700

    refactor: tutorial section

--- src/app/components/TutorialStep.tsx
+++ src/app/components/TutorialStep.tsx
@@ -17,19 +17,19 @@ const TutorialStep = ({
 }: TutorialStepProps) => {
   const borderStyle = active
     ? 'bg-gradient-to-b from-[#EEE2FF] to-[#9747FF]'
-    : 'bg-[#E6E6E680]';
+    : 'bg-gradient-to-b from-border to-border';
   const textStyle = active
-    ? 'bg-gradient-to-br from-[#7F52FF] to-[#FB8570] text-transparent bg-clip-text '
+    ? 'bg-gradient-to-br text-primary-gradient'
     : 'text-black';
 
   return (
     <div
-      className={`${borderStyle} p-[1px] rounded-3xl ${active ? 'shadow-xl shadow-[#6636D214]' : 'hover:shadow-xl hover:shadow-[#6636D214]'} cursor-pointer transition-150`}
+      className={`${borderStyle} p-[1px] rounded-3xl ${active ? 'drop-shadow-[0_4px_30px_#6636D214]' : 'hover:drop-shadow-[0_4px_30px_#6636D214]'} cursor-pointer transition-300`}
       onClick={onClick}
     >
       <div className="bg-white rounded-[23px] py-3 px-4">
         <h3
-          className={` ${textStyle} inline-block font-semibold text-lg mb-2 transition-150`}
+          className={` ${textStyle} inline-block font-semibold text-lg mb-2 transition-300`}
         >
           Step {order} - {title}
         </h3>
--- src/app/components/sections/Tutorial.tsx
+++ src/app/components/sections/Tutorial.tsx
@@ -22,15 +22,15 @@ const Tutorial = () => {
         <Label text="Tutorial" />
         <h2 className="font-nunito font-bold text-2xl md:text-3xl lg:text-4xl text-center leading-10 md:leading-12 lg:leading-14">
           Buat Konten Menarik Untuk Bisnismu <br />{' '}
-          <span className="bg-gradient-to-r from-[#7F52FF] to-[#FB8570] text-transparent bg-clip-text">
+          <span className="bg-gradient-to-r text-primary-gradient">
             Dalam 3 Langkah Mudah!
           </span>
         </h2>
       </div>
 
       <div className="mt-16 flex flex-col lg:flex-row lg:items-center gap-8 lg:gap-12">
-        <div className="lg:basis-1/2 flex items-center justify-center w-full h-64 md:h-[420px] p-2 md:p-12 bg-surface-500 border-1 border-[#E6E6E6] rounded-3xl">
-          <div className="w-full h-full p-1 md:p-8 bg-white border-1 border-[#E6E6E6] rounded-2xl md:rounded-lg">
+        <div className="lg:basis-1/2 flex items-center justify-center w-full h-64 md:h-[420px] p-2 md:p-12 bg-surface-500 border-1 border-border rounded-3xl">
+          <div className="w-full h-full p-1 md:p-8 bg-white border-1 border-border rounded-2xl md:rounded-lg">
             <div className="relative w-full h-full">
               {stepData.map((step, i) => (
                 <Image
